8ef27189274cb0874afbe8562f783dbe6d4142cf
Chamuth Chamandana <c.chamandana@gmail.com>
2023-06-05 09:08:09 +0530

    query handler implementation

---
+++ TallyDB/Server/QueryProcessor/Strategies/IProcessingStrategy.cs
@@ -0,0 +1,9 @@
+﻿using TallyDB.Server.Types;
+
+namespace TallyDB.Server.QueryProcessor.Strategies
+{
+  public interface IProcessingStrategy
+  {
+    public QueryResponse Process(QueryRequest query);
+  }
+}
--- TallyDB/Server/Types/Query.cs
+++ TallyDB/Server/Types/Query.cs
@@ -1,23 +1,28 @@
 ﻿using Newtonsoft.Json;
-using TallyDB.Core;
 
 namespace TallyDB.Server.Types
 {
+  /// <summary>
+  /// Represents a query object
+  /// Combinations of function and object properties determines what action server performs.
+  /// </summary>
   public class Query
   {
     [JsonProperty("function")]
     public string Function { get; set; }
 
-    [JsonProperty("records")]
-    public SliceRecord[]? SliceRecord { get; set; }
-    [JsonProperty("definition")]
-    public SliceDefinition? Definition { get; set; }
+    [JsonProperty("slice")]
+    public SliceCreationData? Slice { get; set; }
+    [JsonProperty("database")] 
+    public DatabaseCreationData? Database { get; set; }
+    [JsonProperty("range")]
+    public QueryRange? Range { get; set; }
+    [JsonProperty("users")]
+    public UserCreationData? Users { get; set; }
 
-    public Query(string function, SliceRecord[]? sliceRecord, SliceDefinition? definition)
+    public Query(string function)
     {
       Function = function;
-      SliceRecord = sliceRecord;
-      Definition = definition;
     }
   }
 }
